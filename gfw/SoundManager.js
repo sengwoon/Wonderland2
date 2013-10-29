c5b4a971f3078ba637da805cb22fdbe58ecdebcb
sengwoon <sengwoon@naver.com>
2013-10-30 03:58:28 +0900

    wonderland v.0.0.41// 사운드 추가 테스트

--- gfw/SoundManager.js
+++ gfw/SoundManager.js
@@ -23,14 +23,13 @@ SoundSystem.prototype.AddSound = function( fileName, resourceCount )
     soundMusic.src = fileName;
     soundMusic.volume = this.volume;
     soundMusic.isPlayed = false;
-    soundMusic.addEventListener("canplaythrough", onLoadSoundComplete, false);
+    //soundMusic.addEventListener("canplaythrough", onLoadSoundComplete, false);
     // soundMusic.addEventListener("ended", function()
     // {
     //   if( window.chrome ) this.load();
     //   this.pause();
     // }, false);  
-    
-    document.body.appendChild(soundMusic);
+    //document.body.appendChild(soundMusic);
     
     this.arrSounds.push( { name: fileName, sound: soundMusic, isPlayed: false  } );
     this.intAllResourceCount++;
